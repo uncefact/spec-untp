28feedbec99471d0aea70f76bcfdf144232ebb78
Kseniya Shychko <kseniya.shychko@gosource.com.au>
2024-10-13 20:06:53 +0200

    feat: add diagrams support

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -132,6 +132,10 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
 };
 
 module.exports = config;
