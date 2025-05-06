4a1bbe8580852486a45217d0427b8c4a71b63345
Ashley Harwood <ashley.harwood@gosource.com.au>
2025-05-06 19:07:31 +1000

    fix: skip validation of secondary instances without context

--- .github/actions/validate-jargon-artefacts/src/contextValidation.js
+++ .github/actions/validate-jargon-artefacts/src/contextValidation.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const jsonld = require('jsonld');
 const { splitSchemasAndInstances } = require('./schemaValidation');
 const { fetchArtefactData } = require('./utils');
+const { JARGON_INSTANCES_TO_VALIDATE, JARGON_CONTEXT_IRI_PREFIX } = require('./config');
 
 /**
  * Validate JSON-LD context
@@ -29,15 +30,41 @@ async function validateContext(jsonldContext) {
 /**
  * Validate JSON-LD context in credentials
  * @param rawArtefactData is an array of JSON schemas and instances
+ * @param jsonldContext is the context object provided by Jargon
  * @returns { valid: boolean }
  */
-async function validateContextInCredential(rawArtefactData) {
+async function validateContextInCredential(rawArtefactData, jsonldContext) {
   const { instances } = splitSchemasAndInstances(rawArtefactData);
   const instanceFileNames = Object.keys(instances);
 
-  // Fetch all instances in parallel
-  const fetchedInstances = await Promise.all(instanceFileNames.map(async instanceFileName => {
+  // Filter out secondary instances that lack @context.
+  const mainInstanceFileNames = instanceFileNames.filter(instanceFileName =>
+    JARGON_INSTANCES_TO_VALIDATE.some(validInstance => instanceFileName.includes(validInstance))
+  );
+
+  if (!mainInstanceFileNames.length) {
+    core.info(`
+      No permitted sample instances found for JSON-LD expansion. 
+      Ensure the instance file name is present in JARGON_INSTANCES_TO_VALIDATE. 
+      Current JARGON_INSTANCES_TO_VALIDATE: ${JARGON_INSTANCES_TO_VALIDATE.join(', ')}
+    `);
+    return { valid: false };
+  }
+
+  // Fetch all permitted instances in parallel
+  const fetchedInstances = await Promise.all(mainInstanceFileNames.map(async instanceFileName => {
     const instanceJson = await fetchArtefactData(instances[instanceFileName]);
+
+    if (instanceJson && jsonldContext && JARGON_CONTEXT_IRI_PREFIX) {
+      const context = instanceJson['@context'];
+
+      if(context && Array.isArray(context) && jsonldContext.url) {
+      // Replace the deployed @context IRI with the Jargon context IRI
+      instanceJson['@context'] = context.map(context =>
+          context.includes(JARGON_CONTEXT_IRI_PREFIX) ? jsonldContext.url : context
+        );
+      }
+    }
 
     return {
       fileName: instanceFileName,
