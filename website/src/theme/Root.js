3d7b5a1e19472a542e40b3df30e3404f7a80d6a4
Ashley Harwood <60303491+ashleythedeveloper@users.noreply.github.com>
2025-07-17 14:36:50 +0200

    feat: add website notice (#509)
    This PR adds a permanent notice to all pages of the website informing
    users that the website has moved, along with a link to the new location.
    <img width="1478" height="1045" alt="Screenshot 2025-07-17 at 10 34
    06 pm"
    src="https://github.com/user-attachments/assets/2cf3f522-8907-4a27-99e8-6c48b04c4899"
    />
    
    Co-authored-by: Ashley Harwood <ashley.harwood@hotmail.com>

--- website/src/theme/Root.js
+++ website/src/theme/Root.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ScrollToHash from '../components/ScrollToHash';
+import WebsiteNotice from '../components/WebsiteNotice';
 import MenuSvgIconsSprite from './svgSprite';
 
 export default function Root({children}) {
   return (
     <>
+      <WebsiteNotice />
       <ScrollToHash />
       {children}
       <MenuSvgIconsSprite />
